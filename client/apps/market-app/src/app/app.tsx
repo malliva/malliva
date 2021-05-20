66c1eeae86e1d896d6fdc3af0145f76102f32905
Joshua Iwugod <iwugodjoshua@gmail.com>
2021-05-20 10:41:00 +0400

    changed market landing page children

--- client/apps/market-app/src/app/app.tsx
+++ client/apps/market-app/src/app/app.tsx
@@ -5,16 +5,12 @@ import star from './star.svg';
 
 import { Route, Link } from 'react-router-dom';
 
-import { MarketAppSignUps } from 'libs/market-app/sign-ups/src/index';
-import { MarketAppSignIn } from 'libs/market-app/sign-in/src/index';
 import { MarketAppLandingPage } from 'libs/market-app/landing-page/src/index';
 
 export function App() {
   return (
     <div>
       <MarketAppLandingPage />
-      <MarketAppSignUps />
-      {/* <MarketAppSignIn /> */}
     </div>
   );
 }
--- client/libs/market-app/landing-page/src/lib/market-app-landing-page.tsx
+++ client/libs/market-app/landing-page/src/lib/market-app-landing-page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './market-app-landing-page.module.scss';
 import { MarketAppTopMenu } from 'libs/market-app/top-menu/src/index';
+import { MarketAppSignUps } from 'libs/market-app/sign-ups/src/index';
+import { MarketAppSignIn } from 'libs/market-app/sign-in/src/index';
 
 /* eslint-disable-next-line */
 export interface MarketAppLandingPageProps {}
@@ -10,6 +12,8 @@ export function MarketAppLandingPage(props: MarketAppLandingPageProps) {
   return (
     <div>
       <MarketAppTopMenu />
+      {/* <MarketAppSignUps /> */}
+      <MarketAppSignIn />
     </div>
   );
 }
