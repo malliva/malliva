161ee34d0ccc8ac1145003d03503f90cc43f0279
Joshua Iwugod <iwugodjoshua@gmail.com>
2021-08-10 00:46:36 +0400

    update top menu and some users right design

--- client/apps/market-app/src/app/store.tsx
+++ client/apps/market-app/src/app/store.tsx
@@ -2,11 +2,24 @@ import { applyMiddleware, createStore } from 'redux';
 import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducers';
 import thunkMiddleware from 'redux-thunk';
-
 import logger from 'redux-logger';
+import { compose } from '@reduxjs/toolkit';
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 const middlewares = [logger, thunkMiddleware];
 
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
 
 export const persistor = persistStore(store);
